ed29ceae9ecaf3eda63a96ee877c65dc39a1d5fb
ashish kalsi <ashishkalsi07@gmail.com>
2024-06-14 23:09:30 +0530

    Updated

---
+++ Array_of_Permutation.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def buildArray(self, nums):
+        ans=[]
+        for i in range(0,len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+
+        
+        
